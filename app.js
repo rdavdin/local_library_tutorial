6b5051aefe85de0cec750bb5ef469ddb7e03a737
rdavdin <rdavdin@gmail.com>
2022-08-24 20:41:59 +0700

    finish! next: prepare to deploy to production

---
+++ app.js
@@ -0,0 +1,48 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const index_router = require('./routes/index');
+const users_router = require('./routes/users');
+const catalog_router = require('./routes/catalog');
+
+const app = express();
+const mongoDB = 'mongodb+srv://minigame:1234@cluster0.nglxfof.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index_router);
+app.use('/users', users_router);
+app.use('/catalog', catalog_router);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ controllers/genre-controller.js
@@ -0,0 +1,220 @@
+const Genre = require('../models/genre');
+const Book = require("../models/book");
+const async = require("async");
+const {body, validationResult} = require('express-validator');
+
+// Display list of all Genre.
+exports.genre_list = (req, res, next) => {
+    Genre.find().sort({
+      name: 1
+    }).exec((err, genres)=>{
+      if(err) {
+        return next(err);
+      };
+      res.render('genre_list', {
+        title: 'Genre List', 
+        genre_list: genres
+      });
+    });
+};
+
+// Display detail page for a specific Genre.
+exports.genre_detail = (req, res, next) => {
+  async.parallel({
+    genre(callback){
+      Genre.findById(req.params.id).exec(callback);
+    },
+    genre_books(callback){
+      Book.find({genre: req.params.id}).exec(callback);
+    }
+  }, (err, results)=>{
+    if(err) {
+      return next(err);
+    }
+    if(results.genre == null){
+      //No results
+      const err = new Error("Genre not found");
+      err.status = 404;
+      return next(err);
+    }
+    //Successful, so render
+    res.render('genre_detail', {
+      title: 'Genre Detail', 
+      genre: results.genre, 
+      genre_books: results.genre_books
+    });
+  });
+};
+
+// Display Genre create form on GET.
+exports.genre_create_get = (req, res) => {
+  res.render('genre_form', {title: "Create Genre"});
+};
+
+// Handle Genre create on POST.
+exports.genre_create_post = [
+  body("name", "Genre name required")
+    .trim()
+    .isLength({min: 1})
+    .escape(),
+  (req, res, next)=>{
+    //Extract the validation errors from a request
+    const errors = validationResult(req);
+
+    //create a genre object with escaped and trimmed data
+    const genre = new Genre({name: req.body.name});
+
+    if(!errors.isEmpty()){
+      res.render('genre_form', {
+        title: "Create Genre",
+        genre,
+        errors: errors.array()
+      });
+      return;
+    }else{
+      Genre.findOne({
+        name: req.body.name
+      }).exec((err, found_genre)=>{
+        if(err) {
+          return next(err);
+        }
+        if(found_genre){
+          //Genre exists, redirect to its detail page
+          res.redirect(found_genre.url);
+        }else{
+          genre.save((err)=>{
+            if(err){
+              return next(err);
+            }
+            //Genre saved. Redirect to genre detail page
+            res.redirect(genre.url);
+          });
+        }
+      });
+    }
+  }
+];
+
+// Display Genre delete form on GET.
+exports.genre_delete_get = (req, res, next) => {
+  async.parallel({
+    genre(callback){
+      Genre.findById(req.params.id).exec(callback);
+    },
+    genre_books(callback){
+      Book.find({genre: req.params.id}, 'title summary').exec(callback);
+    }
+  }, (err, results)=>{
+    if(err){
+      return next(err);
+    }
+    if(results.genre == null){
+      res.redirect('/catalog/genres');
+    }
+
+    res.render('genre_delete', {
+      title: 'Genre Delete',
+      genre: results.genre,
+      genre_books: results.genre_books 
+    });
+  });
+};
+
+// Handle Genre delete on POST.
+exports.genre_delete_post = (req, res, next) => {
+  async.parallel({
+    genre(callback){
+      Genre.findById(req.body.genreid).exec(callback);
+    },
+    genre_books(callback){
+      Book.find({genre: req.body.genreid}, 'title summary').exec(callback);
+    }
+  }, (err, results)=>{
+    if(err){
+      return next(err);
+    }
+    if(results.genre == null){
+      res.redirect('/catalog/genres');
+    }
+    if(results.genre_books.length > 0){
+      res.render('genre_delete', {
+        title: 'Genre Delete',
+        genre: results.genre,
+        genre_books: results.genre_books 
+      });
+    }
+
+    Genre.findByIdAndRemove(req.body.genreid).exec((err)=>{
+      if(err){
+        return next(err);
+      }
+
+      res.redirect('/catalog/genres');
+    });
+  });
+};
+
+// Display Genre update form on GET.
+exports.genre_update_get = (req, res, next) => {
+  Genre.findById(req.params.id).exec((err, genre)=>{
+    if(err){
+      return next(err);
+    }
+    if(genre == null){
+      const err = new Error("Genre not found");
+      err.status = 404;
+      return next(err);
+    }
+    res.render('genre_form', {
+      title: "Update Genre",
+      genre: genre
+    });    
+  });
+};
+
+// Handle Genre update on POST.
+exports.genre_update_post = [
+  body("name", "Genre name required")
+    .trim()
+    .isLength({min: 1})
+    .escape(),
+  (req, res, next)=>{
+    //Extract the validation errors from a request
+    const errors = validationResult(req);
+
+    //create a genre object with escaped and trimmed data
+    const genre = new Genre({
+      name: req.body.name,
+      _id: req.params.id
+    });
+
+    if(!errors.isEmpty()){
+      res.render('genre_form', {
+        title: "Create Genre",
+        genre,
+        errors: errors.array()
+      });
+      return;
+    }
+
+    Genre.findOne({
+      name: req.body.name
+    }).exec((err, found_genre)=>{
+      if(err) {
+        return next(err);
+      }
+      if(found_genre){
+        //Genre exists, redirect to its detail page
+        res.redirect(found_genre.url);
+      }else{
+        Genre.findByIdAndUpdate(req.params.id, genre, {}, (err, thegenre)=>{
+          if(err){
+            return next(err);
+          }
+          //Genre saved. Redirect to genre detail page
+          res.redirect(thegenre.url);
+        });
+      }
+    });
+  }
+];
